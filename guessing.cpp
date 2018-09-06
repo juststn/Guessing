5dd6991509296410dba024b32e59dfffc38eba9e
juststn <42872030+juststn@users.noreply.github.com>
2018-09-05 22:08:02 -0700

    Update guessing.cpp

--- guessing.cpp
+++ guessing.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
 using namespace std;
+
+//Guessing Game by Justin He- guess a number between 0-100
+//the computer will tell you if it is too high or too low or correct
+//if correct your number of guesses will be displayed and have the option
+//to play again
+
 int main() {
-srand(time(NULL));
- int num = rand() %100;
 int input;
  int running = 1;
+ int playing = 1;
+ char yn = ' ';
+ while(playing == 1){
+     srand(time(NULL));
+     int num = rand() %101; //generate random number 0-100
+     int count = 0; //keeps track of guesses
+   running =1;
+   cout << "Input your guess (a number from 0 to 100): " << endl;
  while(running == 1){
    cin >>input;
+   count++; //takes in input and adds to count
  if (input > num){
-   cout << "not equal, greater"<<endl  ; }
+   cout << "Not equal, guess is too high"<<endl  ; }
 
  if (input < num){
-cout << "not equal, less" << endl; }
+    cout << "Not equal, guess is too low" << endl; }
 
- if (input == num){ cout <<"equal"<<endl;
-  running = 0;
+ if (input == num){
+   //if input matches number, total guesses are displayed and option to play again
+  cout <<"Correct"<<endl;
+  cout <<count << " guesses"<<endl << endl;
+  running =0;
+  cout <<"PLAY AGAIN? (Y/N)"<<endl;
+  cin >>yn;
+
+  if(yn=='y'||yn=='Y'){ //restarts game if player answers yes
+    cout <<"RESTARTING"<<endl<<endl;
+    playing = 1;
+  }
+
+  if(yn=='n'||yn=='N'){
+    cout <<"EXITING"<<endl;
+    playing = 0;
+  }
+
+ }
+
+
  }
 }
  return 0;
 }
+
