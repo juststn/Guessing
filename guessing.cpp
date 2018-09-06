7d335624a741d2d9ff678b15f6b8b68f2b70200f
juststn <42872030+juststn@users.noreply.github.com>
2018-09-05 18:51:12 -0700

    Update guessing.cpp

--- guessing.cpp
+++ guessing.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-int num = rand() %100+1;
+srand(time(NULL));
+ int num = rand() %100;
 int input;
-cin >>input;
-if(input != num){
-cout << "not equal" << endl; }
-else { cout <<"equal"<<endl; }
-return 0; }
+ int running = 1;
+ while(running == 1){
+   cin >>input;
+ if (input > num){
+   cout << "not equal, greater"<<endl  ; }
+
+ if (input < num){
+cout << "not equal, less" << endl; }
+
+ if (input == num){ cout <<"equal"<<endl;
+  running = 0;
+ }
+}
+ return 0;
+}
